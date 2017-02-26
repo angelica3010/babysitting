eeb203042624cd0883a8fe0a178325799fed9481
Angel Trinh <trinh.angel@gmail.com>
2017-02-26 18:35:52 -0500

    added handle

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ app.use(session({ secret: 'app', proxy: true, resave: true, saveUninitialized: t
 app.use(cookieParser());
 
 //Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + '/public'));
+// app.use(express.static(process.cwd() + '/public'));
+app.use(express.static('public'))
 
 
 app.use(bodyParser.urlencoded({
